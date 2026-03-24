d68cef5e76a8de14d728ef6f0c9d84011cb554f6
Pixi-Me <ezioboii@gmail.com>
2026-03-24 23:07:59 +0530

    Notes app

--- File System/script.js
+++ File System/script.js
@@ -1,35 +1,85 @@
+const { error } = require('node:console');
 const fs = require('node:fs/promises');
 
-let command = process.argv[2];
-let arg2 = process.argv[3];
-let arg3 = process.argv[4];
-
-
-async function getGata(params) {
-    
+if (process.argv[2] === 'clear') {
+    async function creatingFIle() {
+        try {
+            await fs.writeFile('file.json', "[]")
+            console.log('Cleaned')
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    creatingFIle();
 }
 
-async function addNote() {
-    let note = {
-        title : arg2,
-        data : arg3,
-    };
+else if (process.argv[2] === 'add') {
+    async function adding() {
+        try {
+            if (!process.argv[3] || !process.argv[4]) {
+                throw new Error('Invalid Inputs')
+            }
+            let data = await fs.readFile("./file.json", "utf-8")
+            data = JSON.parse(data)
+            let x = {
+                title: process.argv[3],
+                data: process.argv[4],
+            }
+            data.forEach((e) => {
+                if (e.title === x.title) {
+                    throw new Error('Title Already Exists')
+                }
+            })
+            data.push(x)
+            let final = JSON.stringify(data);
+            await fs.writeFile('./file.json', final);
+            console.log('Added');
+        }
+        catch (e) {
+            console.log(e)
+        }
 
-    let adder = await fs.readFile("./notes.json",'utf-8');
-    let data = JSON.parse(adder);
-    data.push(note);
-    data = JSON.stringify(data);
-    await fs.writeFile('notes.json', data);
-    console.log(data);
-
+    }
+    adding()
+}
+else if (process.argv[2] === 'remove') {
+    async function remove() {
+        try {
+            let data = await fs.readFile('./file.json', 'utf-8')
+            data = JSON.parse(data);
+            data.forEach((e, i) => {
+                if (e.title === process.argv[3]) {
+                    data.splice(i, 1);
+                }
+                else{
+                    throw new Error('Title Does note exist. Try node script.js list')
+                }
+            })
+            data = JSON.stringify(data);
+            
+            await fs.writeFile('./file.json', data);
+        }
+        catch(err){
+            console.log(err)
+        }
+       
+    }
+    remove()
+}
+else if (process.argv[2]==='list'){
+    async function list() {
+        try{
+            let data = await fs.readFile('./file.json','utf-8')
+            data = JSON.parse(data);
+            console.log(data);
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+    list()
 }
 
-async function removeNote(){
-    
-}
 
-if(command==='add'){
-    addNote();
-    
-}
 
