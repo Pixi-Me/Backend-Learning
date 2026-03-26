6ccaac3167cc07e279851d982134aa5652b02b04
Pixi-Me <ezioboii@gmail.com>
2026-03-26 17:42:35 +0530

    Add feature added

--- 0 Practice Projects/tasks/script.js
+++ 0 Practice Projects/tasks/script.js
@@ -1,9 +1,37 @@
 import express from 'express'
 const app = express();
+import * as fs from 'node:fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+async function addTask(obj) {
+    try{
+        let data = await fs.readFile('./file.json','utf-8')
+    data = JSON.parse(data);
+    data.push(obj);
+    data = JSON.stringify(data);
+    await fs.writeFile('file.json', data);
+    return data;
+    }
+    catch(err){
+        console.log(`addTask error: ${err}`)
+    }
+    
+
+}
+async function files(){
+    try{
+        let data = await fs.readFile('file.json','utf-8')
+    data = JSON.parse(data);
+    return data;
+    }
+    catch(err){
+        console.log(`Files error ${err}`)
+    }
+    
+}
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -12,8 +40,15 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res)=>{
-    res.render('index')
+app.get('/',async (req,res)=>{
+    let data = await files();
+    res.render('index',{tasks:data})
+})
+app.post("/submit",async (req,res)=>{
+    await addTask(req.body)
+    let data = await files()
+    res.render('index',{tasks : data})
+    res.redirect('/')
 })
 
 app.listen(3000)
